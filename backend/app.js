7160a8a7c1c0ca4bf2d985cfa8b381f5021c5c8f
eshita201 <eshita201@gmail.com>
2022-07-29 23:17:02 +0530

    add changes

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const connectDb = async=> {
 connectDb();
 app.use(bodyParser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
