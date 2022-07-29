0e5e814088db8bef3a0ee6f79e5d1bcc64aca31f
eshita201 <eshita201@gmail.com>
2022-07-29 20:15:39 +0530

    new build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "postappmean.herokuapp.com" //"http://localhost:3000/api"
+  apiUrl: "http://postappmean.herokuapp.com" //"http://localhost:3000/api"
 };
 
 /*
