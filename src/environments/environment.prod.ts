8a67a0226b91164bc24cedb84f466b8d87c6eda0
eshita201 <eshita201@gmail.com>
2022-07-29 23:09:27 +0530

    added package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   
-  apiUrl: "http://postmeanapp.herokuapp.com/",
+  apiUrl: "http://postmeanapp.herokuapp.com/api",
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://postmeanapp.herokuapp.com/"
+  apiUrl: "http://postmeanapp.herokuapp.com/api"
 };
 
 /*
